c817948bf25a7c7d71d1fa6be7db91bba642192a
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-21 20:27:18 +0200

    Bug with Hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
-import './Home.scss'
+import { useState, useEffect } from 'react';
+
+import './Home.scss';
 
 export default function Home() {
+    // const [counter, setCounter] = useState(0);
+
+    function increment() {
+        // setCounter((prev) => prev + 1);
+    }
+
     return (
         <main className='page-container home'>
-            <h1>Hello World!</h1>
+            <h1>I like the stock!</h1>
+            {/* <p>{counter}</p> */}
+            {/* <button onClick={increment}>Increment</button> */}
         </main>
     );
 }
