d9353f46228f7799987be47d314ee80e992428d8
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-23 17:33:42 +0200

    Style Update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,7 +2,7 @@ import './SignIn.scss';
 
 export default function SignIn () {
     return (
-        <main className='page-container sign-up'>
+        <main className='page-container sign-in'>
             <h1>Sign In with your account!</h1>
         </main>
     )
