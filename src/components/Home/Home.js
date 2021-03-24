4e3f5d92b067af15275eecf9d0c04e05ead77c83
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-25 00:14:50 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,7 @@ import { useState, useEffect } from 'react';
 import './Home.scss';
 
 export default function Home() {
-    let returnValue = '';
-    const [logged, setLogged] = useState(true);
+    const [logged, setLogged] = useState(false);
 
     useEffect(() => {
         setTimeout(() => {
@@ -12,20 +11,9 @@ export default function Home() {
         }, 2000)
     }, []);
 
-    if (logged) {
-        returnValue = (
-            <main className='page-container home'>
-                <h1 className="main-title">HODL!</h1>
-            </main>
-        );
-
-    } else {
-        returnValue = (
-            <main className='page-container home'>
-                <h1 className="main-title">I like the <span className='yellow'>STONK!</span></h1>
-            </main>
-        );
-    }
-
-    return returnValue;
+    return (
+        <main className='page-container home'>
+            <h1 className="main-title">{logged ? "I like the STONK!" : "HODL!" }</h1>
+        </main>
+    );
 }
