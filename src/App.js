73daaa2d161be692b8e6b60e5db21d0ec23c2a54
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-28 11:51:29 +0300

    Remove contact-us page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header/Header';
 import PageFooter from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
-import ContactUs from './components/Contacts/Contacts';
 import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
 
@@ -19,7 +18,6 @@ export default function App() {
         <Switch>
           <Route path='/' exact={true} component={Home}/>
           <Route path='/about-us' exact={true} component={AboutUs}/>
-          <Route path='/contact-us' exact={true} component={ContactUs}/>
           <Route path='/sign-up' exact={true} component={SignUp}/>
           <Route path='/sign-in' exact={true} component={SignIn}/>
         </Switch>
--- src/components/Contacts/Contacts.js
+++
@@ -1,9 +0,0 @@
-import './Contacts.scss';
-
-export default function Contacts () {
-    return (
-        <main className='page-container contact-us'>
-            <h1>Contact Us!</h1>
-        </main>
-    )
-}
