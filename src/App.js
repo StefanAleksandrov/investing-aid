6b19f3a58535d69d07bac8a9aa6631d8362f06e1
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-03-28 17:49:39 +0300

    Add New Record page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
 import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
+import Profile from './components/Profile/Profile';
+import AddRecord from './components/AddRecord/AddRecord';
 
 export default function App() {
   return (
@@ -16,10 +18,12 @@ export default function App() {
         <Header />
 
         <Switch>
-          <Route path='/' exact={true} component={Home}/>
-          <Route path='/about-us' exact={true} component={AboutUs}/>
-          <Route path='/sign-up' exact={true} component={SignUp}/>
-          <Route path='/sign-in' exact={true} component={SignIn}/>
+          <Route path='/' exact={true} component={Home} />
+          <Route path='/about-us' exact={true} component={AboutUs} />
+          <Route path='/sign-up' exact={true} component={SignUp} />
+          <Route path='/sign-in' exact={true} component={SignIn} />
+          <Route path='/profile' exact={true} component={Profile} />
+          <Route path='/add-record' exact={true} component={AddRecord} />
         </Switch>
 
         <PageFooter />
---
+++ src/components/AddRecord/AddRecord.js
@@ -0,0 +1,38 @@
+import { addRecord } from '../../services/stockService';
+import './AddRecord.scss';
+
+export default function CreateRecord({ history }) {
+
+    function create(e) {
+        e.preventDefault();
+
+        const uid = localStorage.getItem("user");
+
+        //If no logged in user abort
+        if (!uid) return history.push('/');
+
+        const [stock, amount, price] = e.target.elements;
+        addRecord(stock.value, amount.value, price.value, uid)
+            .then(() => history.push('/profile'))
+            .catch (console.log);
+    }
+
+    return (
+        <div className="page-container add-record">
+            <form onSubmit={create} >
+                <h1>Add New Investment Record</h1>
+
+                <label htmlFor="stock">Stock</label>
+                <input type="text" id='stock' name='stock' />
+
+                <label htmlFor="amount">Amount</label>
+                <input type="number" id='amount' name='amount' />
+
+                <label htmlFor="price">Price per share in USD</label>
+                <input type="float" id='price' name='price' />
+
+                <input type="submit" value='Add' />
+            </form>
+        </div>
+    )
+}
