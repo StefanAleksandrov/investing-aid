ead5abe3e052332144a03301277651632ee86155
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-04-08 00:17:13 +0300

    Currencies API & Profile component update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,14 +8,14 @@ export default function Profile({
     history,
     currentUser
 }) {
+    const [currency, setCurrency] = useState("USD");
     const [records, setRecords] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
 
     useEffect(() => {
-        const uid = currentUser?.uid || '';
+        if (!currentUser.uid) return;
 
-        if (!uid) return;
-
-        getAll(uid)
+        getAll(currentUser.uid)
             .then(data => {
                 if (data) {
                     let arr = [];
@@ -34,9 +34,16 @@ export default function Profile({
             .catch(console.log);
     }, [currentUser.uid]);
 
+    useEffect(() => {
+        setTotalPrice(() => {
+            return records.reduce((acc, current) => {
+                return acc + current.prices[currency]
+            }, 0);
+        })
+    }, [records, currency]);
+
     function editEntryHandler(e, id) {
         e.preventDefault();
-
         history.push(`/edit/${id}`);
     }
 
@@ -45,14 +52,12 @@ export default function Profile({
 
         if (!window.confirm("Are you sure?")) return;
 
-        const uid = currentUser?.uid || '';
+        if (!currentUser.uid) return;
 
-        if (!uid) return;
-
-        deleteByID(uid, id)
+        deleteByID(currentUser.uid, id)
             .then(resp => {
                 if (resp.statusText === "OK") {
-                    let  newRecords = records.filter(record => {
+                    let newRecords = records.filter(record => {
                         return record.id !== id;
                     })
 
@@ -63,63 +68,72 @@ export default function Profile({
 
     return (
         <div className="page-container">
-            <h1 className="main-heading" >Your Investment records</h1>
+            { records.length < 1
+                    ? <h1 className="main-heading" >No records yet. <Link to='/add-record'>Add one</Link> now!</h1>
+                    : (
+                        <>
+                            <h1 className="main-heading" >Your Investment records</h1>
 
-            <table className="stocks" >
-                <thead className="heading" >
-                    <tr>
-                        <th className="cell" >№</th>
-                        <th className="cell" >Stock</th>
-                        <th className="cell" >Amount</th>
-                        <th className="cell" >Bought date</th>
-                        <th className="cell" >Price USD</th>
-                        <th className="cell" >Price BGN</th>
-                        <th className="cell" >Edit</th>
-                        <th className="cell" >Delete</th>
-                    </tr>
-                </thead>
+                            <table className="stocks" >
+                                <thead className="heading" >
+                                    <tr>
+                                        <th className="cell" >№</th>
+                                        <th className="cell" >Stock</th>
+                                        <th className="cell" >Amount</th>
+                                        <th className="cell" >Bought date</th>
+                                        <th className="cell" >Bought at</th>
+                                        <th className="cell" >Current price</th>
+                                        <th className="cell" >Gain / Loss</th>
+                                        <th className="cell" >Edit</th>
+                                        <th className="cell" >Delete</th>
+                                    </tr>
+                                </thead>
 
-                <tbody className="body" >
-                    {
-                        records.map((record, index) => {
-                            return (
-                                <tr key={record.id} className="line" >
-                                    <td className="cell" >{index + 1}</td>
-                                    <td className="cell" >{record.stock}</td>
-                                    <td className="cell" >{record.amount}</td>
-                                    <td className="cell" >{record.createdAt.split("T")[0]}</td>
-                                    <td className="cell" >{record.price}</td>
-                                    <td className="cell" >{(record.price * 1.65).toFixed(2)}</td>
-                                    <td className="cell btn" >
-                                        <svg onClick={event => editEntryHandler(event, record.id)} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-square" className="svg-inline--fa fa-pen-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 480H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zM238.1 177.9L102.4 313.6l-6.3 57.1c-.8 7.6 5.6 14.1 13.3 13.3l57.1-6.3L302.2 242c2.3-2.3 2.3-6.1 0-8.5L246.7 178c-2.5-2.4-6.3-2.4-8.6-.1zM345 165.1L314.9 135c-9.4-9.4-24.6-9.4-33.9 0l-23.1 23.1c-2.3 2.3-2.3 6.1 0 8.5l55.5 55.5c2.3 2.3 6.1 2.3 8.5 0L345 199c9.3-9.3 9.3-24.5 0-33.9z"></path></svg>
-                                    </td>
-                                    <td className="cell btn" >
-                                        <svg onClick={event => deleteEntryHandler(event, record.id)} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" className="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
+                                <tbody className="body" >
+                                    {
+                                        records.map((record, index) => {
+                                            return (
+                                                <tr key={record.id} className="line" >
+                                                    <td className="cell" >{index + 1}</td>
+                                                    <td className="cell" >{record.stock}</td>
+                                                    <td className="cell" >{record.amount}</td>
+                                                    <td className="cell" >{record.createdAt.split("T")[0]}</td>
+                                                    <td className="cell" >{Number(record.prices[currency]).toFixed(2)} {currency}</td>
+                                                    <td className="cell" >--.-- {currency}</td>
+                                                    <td className="cell" >--.-- %</td>
+                                                    <td className="cell btn" >
+                                                        <svg onClick={event => editEntryHandler(event, record.id)} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-square" className="svg-inline--fa fa-pen-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 480H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zM238.1 177.9L102.4 313.6l-6.3 57.1c-.8 7.6 5.6 14.1 13.3 13.3l57.1-6.3L302.2 242c2.3-2.3 2.3-6.1 0-8.5L246.7 178c-2.5-2.4-6.3-2.4-8.6-.1zM345 165.1L314.9 135c-9.4-9.4-24.6-9.4-33.9 0l-23.1 23.1c-2.3 2.3-2.3 6.1 0 8.5l55.5 55.5c2.3 2.3 6.1 2.3 8.5 0L345 199c9.3-9.3 9.3-24.5 0-33.9z"></path></svg>
+                                                    </td>
+                                                    <td className="cell btn" >
+                                                        <svg onClick={event => deleteEntryHandler(event, record.id)} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" className="svg-inline--fa fa-trash fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
 
-                <tfoot className="foot" >
-                    <tr className="line" >
-                        <td className="cell" ></td>
-                        <td className="cell" ></td>
-                        <td className="cell" ></td>
-                        <td className="cell" >Sum:</td>
-                        <td className="cell" >50.15 USD</td>
-                        <td className="cell" >80.50 BGN</td>
-                        <td className="cell" ></td>
-                        <td className="cell" ></td>
-                    </tr>
-                </tfoot>
-            </table>
-
-            {
-                records.length < 1
-                    ? <h3>No records yet. <Link to='/add-record'>Add one</Link> now!</h3>
-                    : ''
+                                <tfoot className="foot" >
+                                    <tr className="line" >
+                                        <td className="cell" ></td>
+                                        <td className="cell" ></td>
+                                        <td className="cell" ></td>
+                                        <td className="cell" >Sum:</td>
+                                        <td className="cell" >{totalPrice.toFixed(2)} {currency}</td>
+                                        <td className="cell" >--.-- {currency}</td>
+                                        <td className="cell" >--.-- %</td>
+                                        <td className="cell" >Currency:</td>
+                                        <td className="cell" >
+                                            <select className="select-currency" value={currency.toLowerCase()} onChange={(event) => setCurrency(event.target.value.toUpperCase())} >
+                                                <option value="usd">USD</option>
+                                                <option value="eur">EUR</option>
+                                                <option value="bgn">BGN</option>
+                                            </select>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </> )
             }
         </div>
     )
--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,5 +1,5 @@
 import { URL } from '../config/config';
-import { CurrenciesURL } from '../config/config';
+import { CurrenciesURL, ACCESS_KEY } from '../config/config';
 import { auth } from '../config/firebaseInit';
 
 export function getAll(userID) {
@@ -12,23 +12,52 @@ export function getOneByID (userID, id) {
         .then(res => res.json());
 }
 
-export function addRecord(stock, amount, price, uid) {
+export function addRecord(stock, uid) {
     const newRecord = {
-        stock,
-        amount,
-        price,
-        createdAt: new Date()
+        ...stock,
+        createdAt: new Date(),
+        updatedAt: new Date()
     }
 
-    return getCurrencies()
+    return getCurrencies(stock.currency)
         .then(res => res.json())
         .then(data => {
+            const EUR = Number(newRecord.price) / Number(data.rates[newRecord.currency.toUpperCase()]);
+            newRecord.prices = {
+                'EUR': Number(EUR.toFixed(2)),
+                'BGN': Number((EUR * Number(data.rates.BGN)).toFixed(2)),
+                'USD': Number((EUR * Number(data.rates.USD)).toFixed(2))
+            };
+
             newRecord.rates = data.rates;
             return auth.currentUser.getIdToken(false)
         })
         .then((token) => {
             return fetch(URL + `records/${uid}.json?auth=${token}`, {
                 method: "POST",
+                body: JSON.stringify(newRecord)
+            })
+        })
+        .then(res => res.json());
+}
+
+export function updateRecord(stock, uid, id) {
+    const newRecord = {
+        ...stock,
+        updatedAt: new Date()
+    }
+
+    const EUR = Number(newRecord.price) / Number(newRecord.rates[newRecord.currency.toUpperCase()]);
+    newRecord.prices = {
+        'EUR': Number(EUR.toFixed(2)),
+        'BGN': Number((EUR * Number(newRecord.rates.BGN)).toFixed(2)),
+        'USD': Number((EUR * Number(newRecord.rates.USD)).toFixed(2))
+    };
+
+    return auth.currentUser.getIdToken(false)
+        .then((token) => {
+            return fetch(URL + `records/${uid}/${id}.json?auth=${token}`, {
+                method: "PUT",
                 body: JSON.stringify(newRecord)
             })
         })
@@ -44,6 +73,6 @@ export function deleteByID(uid, id) {
         })
 }
 
-function getCurrencies() {
-    return fetch(CurrenciesURL + "?base=USD");
+function getCurrencies(currency) {
+    return fetch(CurrenciesURL + `?access_key=${ACCESS_KEY}`);
 }
