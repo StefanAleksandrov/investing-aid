c2ad40310ca9b308bfd97b55fe00651db5fcf4ae
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-04-09 21:14:56 +0300

    Auth validations and Error messages

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 
 import './SignIn.scss';
 import loginImage from '../../images/login.jpg';
 import { onLogin } from '../../services/authService';
+import { validateEmail, validatePassword } from '../../validators/authValidate';
 
 export default function SignIn () {
     const history = useHistory();
+    const [errorEmail, setErrorEmail] = useState('');
+    const [errorPassword, setErrorPassword] = useState('');
 
     function submitForm(e) {
         e.preventDefault();
 
         const email = e.target.email.value;
-        const pass = e.target.password.value;
+        const password = e.target.password.value;
         const rememberMe = e.target.remember.checked;
 
-        if (!email || !pass) return;
+        const emailError = validateEmail(email);
+        const passwordError = validatePassword(password);
 
-        onLogin(email, pass)
+        setErrorEmail(emailError);
+        setErrorPassword(passwordError);
+
+        if (emailError || passwordError) return;
+
+        onLogin(email, password)
             .then(resp => {
                 if ( rememberMe ) {
                     localStorage.setItem('user', resp.user.uid);
@@ -36,14 +46,16 @@ export default function SignIn () {
                 <form className="form" onSubmit={submitForm} >
                     <h1>Login with your Account</h1>
 
-                    <label>
+                    <label className="error-container" >
                         Email:
-                        <input type="text" name="email" />
+                        <input type="text" name="email" className={errorEmail ? 'error' : ''} />
+                        <div className={"error-message " + (errorEmail ? '' : 'hide') } >{errorEmail}</div>
                     </label>
 
-                    <label>
+                    <label className="error-container" >
                         Password:
-                        <input type="password" name="password" />
+                        <input type="password" name="password" className={errorPassword ? 'error' : ''} />
+                        <div className={"error-message " + (errorPassword ? '' : 'hide') } >{errorPassword}</div>
                     </label>
 
                     <label className="center" >
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,19 +1,37 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './SignUp.scss';
 import registerImage from '../../images/register.jpg';
 import { onRegister } from '../../services/authService';
+import { validateEmail, validateUsername, validatePassword, validateRepeatPassword } from '../../validators/authValidate';
 
 export default function SignUp({ history }) {
+    const [errorEmail, setErrorEmail] = useState('');
+    const [errorUsername, setErrorUsername] = useState('');
+    const [errorPassword, setErrorPassword] = useState('');
+    const [errorRepeatPassword, setErrorRepeatPassword] = useState('');
+
     function submitForm(e) {
         e.preventDefault();
 
         const email = e.target.elements.email.value;
         const username = e.target.elements.username.value;
         const password = e.target.elements.password.value;
+        const repeatPassword = e.target.elements.repassword.value;
+
+        const emailError = validateEmail(email);
+        const usernameError = validateUsername(username);
+        const passwordError = validatePassword(password);
+        const rePasswordError = validateRepeatPassword(password, repeatPassword);
+
+        setErrorEmail(emailError);
+        setErrorUsername(usernameError);
+        setErrorPassword(passwordError);
+        setErrorRepeatPassword(rePasswordError);
 
         //TODO validate
-        if (!email || !username || !password) return;
+        if (emailError || usernameError || passwordError || rePasswordError) return;
 
         //Register user
         onRegister(email, username, password)
@@ -31,24 +49,28 @@ export default function SignUp({ history }) {
                 <form className="form" onSubmit={submitForm} >
                     <h1>Create a New Account</h1>
 
-                    <label>
+                    <label className="error-container" >
                         Email:
-                        <input type="text" name="email" />
+                        <input type="text" name="email" className={errorEmail ? 'error' : ''} />
+                        <div className={"error-message " + (errorEmail ? '' : 'hide') } >{errorEmail}</div>
                     </label>
 
-                    <label>
+                    <label className="error-container" >
                         Username:
-                        <input type="text" name="username" />
+                        <input type="text" name="username" className={errorUsername ? 'error' : ''} />
+                        <div className={"error-message " + (errorUsername ? '' : 'hide') } >{errorUsername}</div>
                     </label>
 
-                    <label>
+                    <label className="error-container" >
                         Password:
-                        <input type="password" name="password" />
+                        <input type="password" name="password" className={errorPassword ? 'error' : ''} />
+                        <div className={"error-message " + (errorPassword ? '' : 'hide') } >{errorPassword}</div>
                     </label>
 
-                    <label>
+                    <label className="error-container" >
                         Repeat Password:
-                        <input type="password" name="re-password" />
+                        <input type="password" name="repassword" className={errorRepeatPassword ? 'error' : ''} />
+                        <div className={"error-message " + (errorRepeatPassword ? '' : 'hide') } >{errorRepeatPassword}</div>
                     </label>
 
                     <label>
