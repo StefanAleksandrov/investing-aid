cafb3c60c00ff39383ca352ddad2a39272fe5209
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-04-09 22:35:19 +0300

    Stock validation implemented

--- src/components/AddRecord/AddRecord.js
+++ src/components/AddRecord/AddRecord.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { addRecord, updateRecord, getOneByID } from '../../services/stockService';
+import validate from '../../validators/stockValidate';
 import './AddRecord.scss';
 
 export default function CreateRecord({
@@ -13,6 +14,10 @@ export default function CreateRecord({
         price: '',
         currency: 'USD',
     });
+
+    const [errorStock, setErrorStock] = useState('');
+    const [errorAmount, setErrorAmount] = useState('');
+    const [errorPrice, setErrorPrice] = useState('');
     
     useEffect(() => {
         if (match.params.id && currentUser) {
@@ -26,6 +31,16 @@ export default function CreateRecord({
 
         //If no logged in user abort
         if (!currentUser.uid) return history.push('/');
+
+        const stockError = validate.stock(stock.stock);
+        const amountError = validate.amount(stock.amount);
+        const priceError = validate.price(stock.price);
+
+        setErrorStock(stockError);
+        setErrorAmount(amountError);
+        setErrorPrice(priceError);
+
+        if (stockError || amountError || priceError) return;
 
         if (match.params.id) {
             updateRecord(stock, currentUser.uid, match.params.id)
@@ -50,21 +65,32 @@ export default function CreateRecord({
                         : <h1 className="main-heading" >Add New Investment Record</h1>
                 }
 
-                <label htmlFor="stock">Stock</label>
-                <input type="text" id='stock' name='stock' value={stock.stock} onChange={(event) => setStock(oldStock => ({...oldStock, stock: event.target.value }))} />
+                <label className="error-container" >
+                    Stock
+                    <input type="text" name='stock' className={errorStock ? 'error' : ''} value={stock.stock} onChange={(event) => setStock(oldStock => ({...oldStock, stock: event.target.value }))} />
+                    <div className={"error-message " + (errorStock ? '' : 'hide') } >{errorStock}</div>
+                </label>
 
-                <label htmlFor="amount">Amount</label>
-                <input type="number" id='amount' name='amount' value={stock.amount} onChange={(event) => setStock(oldStock => ({...oldStock, amount: event.target.value }))} />
+                <label className="error-container" >
+                    Amount
+                    <input type="number" name='amount' className={errorAmount ? 'error' : ''} value={stock.amount} onChange={(event) => setStock(oldStock => ({...oldStock, amount: event.target.value }))} />
+                    <div className={"error-message " + (errorAmount ? '' : 'hide') } >{errorAmount}</div>
+                </label>
 
-                <label htmlFor="price">Price per share</label>
-                <input type="float" id='price' name='price' value={stock.price} onChange={(event) => setStock(oldStock => ({...oldStock, price: event.target.value }))} />
+                <label className="error-container" >
+                    Price per share
+                    <input type="float" name='price' className={errorPrice ? 'error' : ''} value={stock.price} onChange={(event) => setStock(oldStock => ({...oldStock, price: event.target.value }))} />
+                    <div className={"error-message " + (errorPrice ? '' : 'hide') } >{errorPrice}</div>
+                </label>
 
-                <label htmlFor="currency">Currency</label>
-                <select id='currency' name='currency' value={stock.currency} onChange={(event) => setStock(oldStock => ({...oldStock, currency: event.target.value }))} >
-                    <option value="usd" >USD</option>
-                    <option value="eur" >EUR</option>
-                    <option value="bgn" >BGN</option>
-                </select>
+                <label className="error-container" >
+                    Currency
+                    <select name='currency' value={stock.currency} onChange={(event) => setStock(oldStock => ({...oldStock, currency: event.target.value }))} >
+                        <option value="usd" >USD</option>
+                        <option value="eur" >EUR</option>
+                        <option value="bgn" >BGN</option>
+                    </select>
+                </label>
 
                 {
                     match.params.id
---
+++ src/validators/stockValidate.js
@@ -0,0 +1,22 @@
+export default {
+    stock(stock) {
+        if (stock === '') return 'Stock field should not be empty!';
+        if (stock.length < 3) return 'Stock should be 3 symbols or more!'
+
+        return;
+    },
+
+    amount(amount) {
+        if (amount === '') return 'Amount field should not be empty!';
+        if (amount <= 0) return 'Amount should be a positive number!';
+
+        return;
+    },
+
+    price(price) {
+        if (price === '') return 'Price per share field should not be empty!';
+        if (Number(price) === NaN) return 'Price should be a number!';
+
+        return;
+    }
+}
