db3522c8a4b80bad5fbc5a11a53a3972e491d0fa
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-04-11 00:00:07 +0300

    FIX redirect + minor changes

--- src/components/NavItem/NavItem.js
+++ src/components/NavItem/NavItem.js
@@ -1,42 +1,33 @@
 import { Component } from 'react';
-import { NavLink, Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { auth } from '../../config/firebaseInit';
+import AuthContext from '../../contexts/AuthContext';
 
 import './NavItem.scss';
 
 export default class NavItem extends Component {
+    static contextType = AuthContext;
+
     constructor(props) {
         super(props);
-        this.state = {
-            redirect: false,
-        }
 
         this.signOut = this.signOut.bind(this);
-        this.renderRedirect = this.renderRedirect.bind(this);
     }
 
     signOut(e) {
         e.preventDefault();
-
-        if (localStorage.user || localStorage.email || localStorage.username ) {
-            localStorage.removeItem('user');
+        
+        if (localStorage.uid || localStorage.email || localStorage.username ) {
+            localStorage.removeItem('uid');
             localStorage.removeItem('email');
             localStorage.removeItem('username');
         }
-
-        this.setState((state) => ({
-            ...state,
-            redirect: true,
-        }));
-
+        
+        //set auth context to empty strings
+        this.context[1](() => ({email: '', uid: '', username:''}) );
         auth.signOut();
-    }
-
-    renderRedirect() {
-        if (this.state.redirect) {
-            return <Redirect to='/' />
-        }
+        this.props.history.push('/');
     }
 
     render() {
@@ -46,14 +37,12 @@ export default class NavItem extends Component {
                     return (
                         <li className='nav-item' key={x.id}>
                             { x.link === '/sign-out'
-                                ? <a href='/' onClick={this.signOut} >{x.title.toUpperCase()}</a>
-                                : <NavLink to={x.link} >{x.title.toUpperCase()}</NavLink>
+                                ? <a href='/' onClick={this.signOut} >{x.title}</a>
+                                : <NavLink to={x.link} >{x.title}</NavLink>
                             }
                         </li>
                     )
                 })}
-
-                { this.renderRedirect() }
             </ul>
         )
     }
