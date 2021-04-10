d44b8bbcddd01e1999242218110c376a6a105cd7
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2021-04-10 12:39:45 +0300

    Import and Provide Contexts

--- src/App.js
+++ src/App.js
@@ -20,33 +20,42 @@ import CurrencyRates from './components/CurrencyRates/CurrencyRates';
 import PageNotFound from './components/PageNotFound/PageNotFound';
 import Notification from './components/Notification/Notification';
 
+//Context
+import NotificationContext from './contexts/NotificationContext';
+import AuthContext from './contexts/AuthContext';
+
 export default function App() {
-  const [ currentUser, setCurrentUser ] = useState(null);
-  const [notification, dispatch] = useReducer(notificationReducer, {
-      message: '',
-      type: ''
+  const [currentUser, setCurrentUser] = useState({
+    uid: '',
+    email: '',
+    username: ''
   });
 
-  function notificationReducer (state, action) {
-      switch (action.action) {
-          case 'NOTIFY':
-              setTimeout(() => {
-                  dispatch({message:'', type:'', action: 'REMOVE'});
-              }, 3000);
+  const [notification, dispatch] = useReducer(notificationReducer, {
+    message: '',
+    type: ''
+  });
 
-              return {...state, message: action.message, type: action.typej}
-          case 'REMOVE':
-              return {...state, message: '', type: ''}
-          default:
-              return state;
-      }
+  function notificationReducer(state, action) {
+    switch (action.action) {
+      case 'NOTIFY':
+        setTimeout(() => {
+          dispatch({ message: '', type: '', action: 'REMOVE' });
+        }, 3000);
+        const newState = { ...state, message: action.message, type: action.type }
+        return newState;
+
+      case 'REMOVE':
+        return { ...state, message: '', type: '' }
+
+      default:
+        return state;
+    }
   }
-
+  
   useEffect(() => {
-    auth.onAuthStateChanged( setCurrentUser );
-  }, []);
+    // auth.onAuthStateChanged(setCurrentUser);
 
-  useEffect(() => {
     if (localStorage.user || localStorage.email) {
       setCurrentUser({
         uid: localStorage.user,
@@ -55,30 +64,33 @@ export default function App() {
       })
     }
   }, []);
-  
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Header currentUser={currentUser} />
-        <Notification message={notification.message} type={notification.type} />
+        <AuthContext.Provider value={[currentUser, setCurrentUser]} >
+          <NotificationContext.Provider value={[notification, dispatch]}>
+            <Header />
+            <Notification />
 
-        <Switch>
-          <Route path='/' exact={true} component={Home} />
-          <Route path='/about-us' exact={true} component={AboutUs} />
-          {/* <Route path='/sign-up' exact={true} component={SignUp} /> */}
-          <Route path='/sign-up' exact={true} render={(props) => <SignUp {...props} dispatch={dispatch} />} />
-          <Route path='/sign-in' exact={true} component={SignIn} />
-          <Route path='/profile' exact={true} render={(props) => <Profile {...props} currentUser={currentUser} />} />
-          
-          {/* In order to pass the currentUser as props WITH the Router props  we need to use the render method with arrow function */}
-          <Route path='/add-record' exact={true} key="add" render={(props) => <AddRecord {...props} currentUser={currentUser} />} />
-          <Route path='/edit/:id' exact={true} key="edit" render={(props) => <AddRecord {...props} currentUser={currentUser} />} />
+            <Switch>
+              <Route path='/' exact={true} component={Home} />
+              <Route path='/about-us' exact={true} component={AboutUs} />
+              <Route path='/sign-up' exact={true} component={SignUp} />
+              <Route path='/sign-in' exact={true} component={SignIn} />
+              <Route path='/profile' exact={true} render={(props) => <Profile {...props} currentUser={currentUser} />} />
 
-          <Route path='/currency-rates' exact={true} component={CurrencyRates} />
-          <Route path='*' exact={true} component={PageNotFound} />
-        </Switch>
+              {/* In order to pass the currentUser as props WITH the Router props  we need to use the render method with arrow function */}
+              <Route path='/add-record' exact={true} key="add" render={(props) => <AddRecord {...props} currentUser={currentUser} />} />
+              <Route path='/edit/:id' exact={true} key="edit" render={(props) => <AddRecord {...props} currentUser={currentUser} />} />
 
-        <PageFooter />
+              <Route path='/currency-rates' exact={true} component={CurrencyRates} />
+              <Route path='*' exact={true} component={PageNotFound} />
+            </Switch>
+
+            <PageFooter />
+          </NotificationContext.Provider>
+        </AuthContext.Provider>
       </ BrowserRouter>
     </div>
   );
